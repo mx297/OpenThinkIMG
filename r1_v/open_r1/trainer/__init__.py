f13b6977dfb4ef5ab755f5db907978303c835330
Mohamed Atef <matef297@outlook.com>
2026-04-06 08:11:31 +0200

    Wire step judge reward entrypoints

--- r1_v/open_r1/trainer/__init__.py
+++ r1_v/open_r1/trainer/__init__.py
@@ -3,6 +3,10 @@ from .vllm_grpo_trainer import Qwen2VLGRPOVLLMTrainer
 from .tool_grpo_trainer import Qwen2VLGRPOTrainer as Qwen2VLGRPOToolTrainer
 from .tool_vllm_grpo_trainer import Qwen2VLGRPOVLLMTrainer as Qwen2VLGRPOToolVLLMTrainerLegacy
 from .tool_vllm_grpo_trainer_safe import Qwen2VLGRPOVLLMTrainer as Qwen2VLGRPOToolVLLMTrainerSafe
+from .tool_vllm_grpo_trainer_step_judge import (
+    Qwen2VLGRPOStepJudgeVLLMTrainerLegacy,
+    Qwen2VLGRPOStepJudgeVLLMTrainerSafe,
+)
 
 Qwen2VLGRPOToolVLLMTrainer = Qwen2VLGRPOToolVLLMTrainerSafe
 
@@ -13,4 +17,6 @@ __all__ = [
     "Qwen2VLGRPOToolVLLMTrainer",
     "Qwen2VLGRPOToolVLLMTrainerLegacy",
     "Qwen2VLGRPOToolVLLMTrainerSafe",
+    "Qwen2VLGRPOStepJudgeVLLMTrainerLegacy",
+    "Qwen2VLGRPOStepJudgeVLLMTrainerSafe",
 ]
